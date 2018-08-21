6acd6d5607b652b490b41821498878e068d8c40d
LinFordLee <LinFordLee@163.com>
2018-08-21 10:40:54 +0800

    spring设计模式

--- src/com/linford/vip/springpattern/springTemplate/MemberDaoTest.java
+++ src/com/linford/vip/springpattern/springTemplate/MemberDaoTest.java
@@ -1,8 +1,16 @@
 package com.linford.vip.springpattern.springTemplate;
-
-
 import com.linford.vip.springpattern.springTemplate.dao.MemberDao;
 
+/**
+ * 模板方法模式
+ * 定义一个操作中的算法的骨架，而将一些步骤延迟到子类中，
+ * Template Method 使得子类可以不改变一个算法的结构
+ * 即可重定义该算法的某些特定步骤。例如JDBCTemplate
+ * 属于行为型模式
+ * 特点：执行流程固定，但中间有些步骤有细微差别(运行时才确定)，
+ * 可实现批量生产。
+ * 穷举：springORM数据模型
+ */
 public class MemberDaoTest {
     public static void main(String[] args) {
         MemberDao memberDao = new MemberDao();
