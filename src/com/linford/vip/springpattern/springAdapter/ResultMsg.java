515ff26b2400bdb05594b604ad3f040b9cd62c7b
LinFordLee <LinFordLee@163.com>
2018-08-25 16:23:10 +0800

    spring设计模式

--- src/com/linford/vip/springpattern/springAdapter/ResultMsg.java
+++ src/com/linford/vip/springpattern/springAdapter/ResultMsg.java
@@ -1,21 +1,21 @@
 package com.linford.vip.springpattern.springAdapter;
 
 public class ResultMsg {
-    private String code;
+    private int code;
     private String msg;
     private  Object data;
 
-    public ResultMsg(String code, String msg, Object data) {
+    public ResultMsg(int code, String msg, Object data) {
         this.code = code;
         this.msg = msg;
         this.data = data;
     }
 
-    public String getCode() {
+    public int getCode() {
         return code;
     }
 
-    public void setCode(String code) {
+    public void setCode(int code) {
         this.code = code;
     }
 
